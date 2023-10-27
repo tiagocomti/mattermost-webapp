088dcd7cd8eb1dbd58a5e0c998064abcaec4645d
tiago <tiago.alexandre.oliveira@hotmail.com>
2023-10-27 13:19:39 -0300

    ajustando forma de que gera o canal de agf analise

--- components/create_post/create_post.jsx
+++ components/create_post/create_post.jsx
@@ -1497,7 +1497,7 @@ class CreatePost extends React.PureComponent {
             scrollbarClass = ' scroll';
         }
         if(currentChannel.name !== "radar") {
-            if(currentChannel.name === "insights" || currentChannel.name === "central"){
+            if(currentChannel.name === "insights" || currentChannel.name === "analise"){
                 return (
                     <SystemPermissionGate permissions={[Permissions.USER_STATUS_MENTOR]}>
                         <form
